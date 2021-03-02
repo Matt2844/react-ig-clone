d79522cade1d60e2fb12fd292b1e653fd73311e4
Matt2844 <matt.hd.ford@gmail.com>
2021-03-02 14:38:59 -0500

    setup project

---
+++ src/components/signin-btn/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+export default function SignInBtn () {
+  return (
+    <div className="sign-in-btn">
+      <button>Sign In With Google</button>
+    </div>
+  )
+}
